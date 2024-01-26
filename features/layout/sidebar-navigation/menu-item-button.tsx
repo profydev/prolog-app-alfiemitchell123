779e8939fdced6d891ed232bae2a3246fce5aae4
alfiemitchell123 <alfiemitchell1@gmail.com>
2024-01-26 16:46:24 -0700

    Fixed the sidebar nav arrow, so it rotates when the sidebar is collapsed

--- features/layout/sidebar-navigation/menu-item-button.tsx
+++ features/layout/sidebar-navigation/menu-item-button.tsx
@@ -9,6 +9,7 @@ type MenuItemProps = {
   iconSrc: string;
   onClick: () => void;
   isCollapsed: boolean;
+  rotateIcon: boolean;
 };
 
 export function MenuItemButton({
@@ -17,12 +18,20 @@ export function MenuItemButton({
   onClick,
   iconSrc,
   isCollapsed,
+  rotateIcon = false,
 }: MenuItemProps) {
   return (
     <li className={classNames(styles.listItem, className)}>
       <Button className={styles.anchor} onClick={onClick}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img className={styles.icon} src={iconSrc} alt={`${text} icon`} />{" "}
+        <img
+          className={styles.icon}
+          style={
+            rotateIcon && isCollapsed ? { transform: "rotate(180deg" } : {}
+          }
+          src={iconSrc}
+          alt={`${text} icon`}
+        />{" "}
         {!isCollapsed && text}{" "}
       </Button>
     </li>
