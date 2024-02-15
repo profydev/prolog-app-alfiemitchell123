63b1f46d7a840bc5fd0932c1cf4564bc4bf2fc9b
alfiemitchell123 <alfiemitchell1@gmail.com>
2024-02-14 19:17:04 -0800

    Created button looks in Storybook

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,9 +1,68 @@
-import { ButtonHTMLAttributes } from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+export enum ButtonSize {
+  sm = "sm",
+  md = "md",
+  lg = "lg",
+  xl = "xl",
+}
+
+export enum ButtonColor {
+  primary = "primary",
+  secondary = "secondary",
+  gray = "gray",
+  empty = "empty",
+  emptyGray = "emptyGray",
+  error = "error",
+  emptyError = "emptyError",
+}
+
+export enum ButtonIcon {
+  leading = "leading",
+  trailing = "trailing",
+  only = "only",
+  none = "none",
+}
+
+type ButtonProps = {
+  children: React.ReactNode;
+  icon?: ButtonIcon;
+  iconSrc?: string;
+  size?: ButtonSize;
+  color?: ButtonColor;
+  onClick?: () => void;
+  className?: string;
+};
+
+export function Button({
+  children,
+  iconSrc,
+  icon = ButtonIcon.none,
+  size = ButtonSize.md,
+  color = ButtonColor.primary,
+  onClick,
+}: ButtonProps) {
   return (
-    <button {...props} className={classNames(styles.button, props.className)} />
+    <button
+      className={classNames(
+        styles.button,
+        styles[size],
+        styles[color],
+        icon === ButtonIcon.only && styles.only,
+      )}
+      onClick={onClick}
+    >
+      {icon === ButtonIcon.leading && (
+        <img className={classNames(styles.icon)} src={iconSrc} />
+      )}
+      {icon !== ButtonIcon.only && children}
+      {icon === ButtonIcon.trailing && (
+        <img className={classNames(styles.icon)} src={iconSrc} />
+      )}
+      {icon === ButtonIcon.only && (
+        <img className={classNames(styles.icon)} src={iconSrc} />
+      )}
+    </button>
   );
 }
