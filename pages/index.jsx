0b5f7547b72b57ae5de061e1a0c68287bd3df90e
alfiemitchell123 <alfiemitchell1@gmail.com>
2024-07-19 19:35:13 -0600

    updated header of landing page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,37 @@
 import { Routes } from "@config/routes";
+import { Button, ButtonSize } from "@features/ui";
+import Link from "next/link";
 import styles from "./index.module.scss";
 
 const IssuesPage = () => {
+  const menuItems = [
+    { text: "Home", href: "/" },
+    { text: "Products", href: "/products" },
+    { text: "Documentation", href: "/documentation" },
+    { text: "Pricing", href: "/pricing" },
+  ];
+
   return (
     <div>
       <header className={styles.header}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <ul className={styles.navLinks}>
+          {menuItems.map((menuItem, index) => (
+            <li key={index}>
+              <Link className={styles.navLink} href={menuItem.href}>
+                {menuItem.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Button
+          size={ButtonSize.lg}
+          className={styles.dashboardLink}
+          href={Routes.projects}
+        >
+          Open Dashboard
+        </Button>
       </header>
       <button
         className={styles.contactButton}
